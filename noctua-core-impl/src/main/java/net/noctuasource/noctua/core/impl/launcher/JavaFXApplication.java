72c16a16ed7fa24c0aa2a9be933849c02beb70aa
thomy <thomy@thomylap.floaz.de>
2014-04-19 18:01:20 +0200

    JavaFX 8 ready.

--- noctua-core-impl/src/main/java/net/noctuasource/noctua/core/impl/launcher/JavaFXApplication.java
+++ noctua-core-impl/src/main/java/net/noctuasource/noctua/core/impl/launcher/JavaFXApplication.java
@@ -56,6 +56,8 @@ public class JavaFXApplication extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		setUserAgentStylesheet(STYLESHEET_CASPIAN);
+		
 		Platform.setImplicitExit(false);
 
 		LoadingScreenManager.get().showSplashLoadingScreen();
